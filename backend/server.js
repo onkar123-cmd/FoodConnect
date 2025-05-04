9b84087c50f1aa14067ddb5ac9d1a2c0ab0fed89
onkar123-cmd <omhui1234@gmail.com>
2025-05-05 00:36:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ const app=express()
 const port=process.env.PORT || 4000
 
 // Allow multiple origin
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['https://food-connect-frontend.vercel.app']
 
 // middleware
 app.use(express.json())
-app.use(cors()) // {origin: allowedOrigins, credentials: true}
+app.use(cors({origin: allowedOrigins, credentials: true})) // {origin: allowedOrigins, credentials: true}
 
 // db connection
 connectDB();
